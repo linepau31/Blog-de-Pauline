3ff0a36e40db7e4a0cccd9abdef1a11c7808c27b
pauline marot <pauline.marot2@outlook.fr>
2022-07-18 16:51:36 +0200

    template home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,25 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
+        //dd($articles);
+
+        return $this->render('home/index.html.twig', [
+            'articles' => $articles,
+        ]);
     }
+
+    #[Route('/show/{id}', name: 'app_article')]
+    public function show($id): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->find($id);
+
+        return $this->render('article/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+
 }
